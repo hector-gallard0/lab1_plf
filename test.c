c5bd3930b2e40baa88e49de5bc7f6620ec55611e
hector-gallard0 <hector.gallardo.a@usach.cl>
2023-04-09 21:11:58 -0400

    cambio de la funcion de analisis lexico

--- test.c
+++ test.c
@@ -2,9 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *componentes[] = {"IDENTIFICADOR", "ENTERO", "REAL EN NOTACION DECIMAL", "REAL EN NOTACION EXPONENCIAL", "SIMBOLO", "PALABRA RESERVADA"};
 
-int esIdentificador(char c_str[]){
+int isLowerLetter(char c_str[]){
     if(strcmp(c_str, "a") == 0 || strcmp(c_str, "b") == 0 || strcmp(c_str, "c") == 0 || strcmp(c_str, "d") == 0 
     || strcmp(c_str, "e") == 0 || strcmp(c_str, "f") == 0 || strcmp(c_str, "g") == 0 || strcmp(c_str, "h") == 0
     || strcmp(c_str, "i") == 0 || strcmp(c_str, "j") == 0 || strcmp(c_str, "k") == 0 || strcmp(c_str, "l") == 0
@@ -18,7 +17,7 @@ int esIdentificador(char c_str[]){
     return 0;
 }
 
-int esEntero(char c_str[]){
+int esDigito(char c_str[]){
     if(strcmp(c_str, "0") == 0 || strcmp(c_str, "1") == 0 || strcmp(c_str, "2") == 0 || strcmp(c_str, "3") == 0
     || strcmp(c_str, "4") == 0 || strcmp(c_str, "5") == 0 || strcmp(c_str, "6") == 0 || strcmp(c_str, "7") == 0
     || strcmp(c_str, "8") == 0 || strcmp(c_str, "9") == 0){
@@ -54,74 +53,41 @@ int esExponencial(char c_str[]){
     return 0;
 }
 
-int esPalabraReservada(char c_str[]){
+int isUpperLetter(char c_str[]){
     if(strcmp(c_str, "A") == 0 || strcmp(c_str, "B") == 0 || strcmp(c_str, "C") == 0 || strcmp(c_str, "D") == 0 
     || strcmp(c_str, "E") == 0 || strcmp(c_str, "F") == 0 || strcmp(c_str, "G") == 0 || strcmp(c_str, "H") == 0
     || strcmp(c_str, "I") == 0 || strcmp(c_str, "J") == 0 || strcmp(c_str, "K") == 0 || strcmp(c_str, "L") == 0
     || strcmp(c_str, "M") == 0 || strcmp(c_str, "H") == 0 || strcmp(c_str, "O") == 0 || strcmp(c_str, "P") == 0
     || strcmp(c_str, "Q") == 0 || strcmp(c_str, "R") == 0 || strcmp(c_str, "S") == 0 || strcmp(c_str, "T") == 0
     || strcmp(c_str, "U") == 0 || strcmp(c_str, "V") == 0 || strcmp(c_str, "w") == 0 || strcmp(c_str, "Y") == 0 
-    || strcmp(c_str, "Z") == 0 ){
-        return 1;
-    }
+    || strcmp(c_str, "Z") == 0 ) return 1;
 
     return 0;
 }
 
-char *buscarComponente(char c_str[]){
-    if(esIdentificador(c_str) == 1){
-        return "IDENTIFICADOR";
-    }else if(esEntero(c_str) == 1){
-        return "ENTERO";
-    }
-    else if(esSimbolo(c_str) == 1){
-        return "SIMBOLO";
-    }
-    // else if(esPalabraReservada() == 1){
-
-    // }
-    else if(esDecimal(c_str) == 1){
-        return "REAL EN NOTACION DECIMAL";
-    }
-    else if(esExponencial(c_str) == 1){
-        return "REAL EN NOTACION EXPONENCIAL";
-    }
-    else if(esPalabraReservada(c_str) == 1){
-        return "PALABRA RESERVADA";
-    }
-
-    return "NULL";
+void concat(char **text, char c_str[]){
+    *text = realloc(*text, (sizeof(char) * (strlen(*text) + strlen(c_str) + 1)));
+    strcat(*text, c_str);
 }
 
-void analizarTexto(char **text, char current_c_str[], char **current_component, FILE *fp){
-    int next_c = fgetc(fp);
-    char next_c_str[2] = {next_c, '\0'};
-    char *next_component;
-    // printf("hola" );
-    next_component = buscarComponente(current_c_str);
-    printf("next component -> %s\n", next_component);
-    // printf("%s", next_component);
-    // Si el componente de la cadena es igual al componente del caracter
-    if(strcmp(*current_component, next_component) != 0){        
-        // printf("son distintos");
-        //Si es identificador
-        if(strcmp(*current_component, componentes[0]) == 0){
-            // printf("IDENTIFICADOR -> %s", *text);
+int isValid(char c_str[]){
+    if(isLowerLetter(c_str) == 1 || esDigito(c_str)  == 1 || esSimbolo(c_str) == 1 || strcmp(c_str, ",") == 0 || strcmp(c_str, "E") == 0
+    || isUpperLetter(c_str) == 1) return 1;
+
+    return 0;
+}
+
+void lexicalAnalysis(char c_str[], char next_c_str[], char **text, char **text_component){
+    printf("%s, %s\n", c_str, next_c_str);
+
+    if(isLowerLetter(c_str) == 1){
+        if(isLowerLetter(next_c_str) == 1){
+            concat(&text, c_str);
         }
-    }else{
-        //si el componente es un identificador
-        if(strcmp(*current_component, componentes[0]) == 0){
-            *text = realloc(*text, (sizeof(char) * (strlen(*text) + strlen(current_c_str) + 1)));
-            strcat(*text, current_c_str);    
-            fseek(fp, -1L, SEEK_CUR);
+        else if(isUpperLetter(c_str) == 1){
+
         }
     }
-    
-    // printf("son iguales");
-    // *text = realloc(*text, (sizeof(char) * (strlen(*text) + strlen(current_c_str) + 1)));
-    // strcat(*text, current_c_str);    
-    // fseek(fp, -1L, SEEK_CUR);
-    
 }
 
 int main(int argc, char* argv[]){    
@@ -136,22 +102,27 @@ int main(int argc, char* argv[]){
 
     char *text = (char*)malloc(sizeof(char));
     strcpy(text, "");
-    int i = 2;
-    char *componente = componentes[0];      
-    // printf("%s", componente);     
-    while ((c = fgetc(fp)) != EOF) {        
-        char c_str[2] = {c, '\0'};  
-        // printf("%s", c_str);
-        analizarTexto(&text, c_str, &componente, fp);        
-        // modificar(&text, c_str);
-        // printf("%s", text);
-        // break;
-        // text = realloc(text, (sizeof(char)*i));
-        // text = realloc(text, (sizeof(char)*(strlen(text) + 2)));                        
-        // strcat(text, c_str);            
-        // i = i + 1;
-    }
+    char *text_component = (char*)malloc(sizeof(char));
+    strcpy(text_component, "");
 
+    int inicio = 1;
+          
+    while ((c = fgetc(fp)) != EOF) {            
+        char c_str[2] = {c, '\0'};    
+        int next_c = fgetc(fp);
+        char next_c_str[2] = {next_c, '\0'};
+
+        if(isValid(c_str) == 1){
+            lexicalAnalysis(c_str, next_c_str, &text, &text_component);
+        }    
+
+        if(next_c == EOF){
+            break;
+        }
+        
+        
+        fseek(fp, -1, SEEK_CUR);
+    }    
     printf("\n%s", text);
     free(text);
     fclose(fp);
